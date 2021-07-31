f8d72ac4127388a12dc1c71e5be7c7e6038f2bb8
Shemanta Bhowmik <61617847+shemanta-bhowmik@users.noreply.github.com>
2021-07-31 20:20:26 +0700

    Version 1.0.1
    Just a simple condition added on the functions.php file.

--- modern-comet/functions.php
+++ modern-comet/functions.php
@@ -7,3 +7,10 @@
 
     }
     add_action( 'wp_enqueue_scripts', 'modern_comet_styles_func' );
+
+    // just a test condition
+    if ( murir_tin == gorur_ghu ) {
+        echo 'Kuttar Mut';
+    } else {
+        echo 'Kuttar Ghu';
+    }
